ffff00aacb2debed06c0b10fde9bb497bde1a1a5
skrp <rich.tijerina@gmail.com>
2016-12-01 23:18:47 -0700

    Create chronic_integrity.pl

---
+++ chronic_integrity.pl
@@ -0,0 +1,29 @@
+#!/usr/local/bin/perl
+# SCRUB CRONJOB
+# skrp Prince of Archives
+use strict;
+use warnings;
+use Digest::SHA ();
+use File::Find::Rule;
+###############################
+# USAGE
+my ( $dir, $log) = @ARGV;
+open(my $lfh, '>>', $log) or die("Can't open $log\n");
+###############################
+# BEGIN
+my $rule=File::Find::Rule->file()->start($dir);
+while (defined(my $file=$rule->match)) {
+      my $orgi = $file;
+      my $sha = file_digest($file) or die "couldn't sha $file";
+      if ($sha eq $orgi) {
+            next;
+      }
+      else {
+            print {$lfh} "$file: ALERT FKN ENTROPY!\n";
+      }
+}
+sub file_digest {
+  my ($filename) = @_;
+  my $digester = Digest::SHA->new('sha256');
+  return $digester->hexdigest;
+}
