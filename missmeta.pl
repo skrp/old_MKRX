b5ec0a386f6b1c104d48b2dea4431b01058062f8
skrp <rich.tijerina@gmail.com>
2016-12-01 20:36:26 -0700

    Create missmeta.pl

---
+++ missmeta.pl
@@ -0,0 +1,50 @@
+#!/usr/local/bin/perl
+# LOG ALL FILES MISSING A CORESPONDING META FILE
+# skrp Prince of Archives
+use strict;
+use warnings;
+use Parallel::ForkManager;
+use List::Utils 'any';
+###############################
+# USAGE
+my ( $input, $masterlist) = @ARGV;
+open(my $sfh, '<', $input) or die("Can't open $input\n");
+open(my $mfh, '<', $masterlist) or die("Can't open $masterlist\n");
+################################
+# JOBS
+use constant MAX_PROC => 4;
+use constant JOBS => 10000;
+my $manager = Parallel::ForkManager->new(MAX_PROC);
+$manager->set_waitpid_blocking_sleep(0);
+################################
+# ARRAYS
+my @files = <$sfh>;
+my @master = readline $mfh;
+chomp @files;
+###############################
+# BEGIN
+my @spool;
+foreach my $file (@files) {
+  push @spool, my $file;
+  run_spooled() if JOBS <= @spool;
+}
+run_spooled() if @spool;
+$manager->wait_all_children;
+###############################
+# MAIN FN
+sub run_spooled {
+  my (@jobs) = splice @spool, 0, JOBS, ();
+  my pid = $manager->start and return;
+  for my $file (@jobs) {
+      $file =~ s/g//;
+      if( any {$file} @master ) {
+          next;
+      }
+      else {
+          print "$file is not in $masterlist\n";
+      }
+  }
+  $manager->finish;
+close $sfh;
+close $mfh;
+}
