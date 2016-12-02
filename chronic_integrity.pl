5d61ccca6cc166389ca23e9a3bae42f62cd9f8c5
skrp <rich.tijerina@gmail.com>
2016-12-02 14:50:45 -0700

    Update chronic_integrity.pl

--- chronic_integrity.pl
+++ chronic_integrity.pl
@@ -7,14 +7,14 @@ use Digest::SHA ();
 use File::Find::Rule;
 ###############################
 # USAGE
-my ( $dir, $log) = @ARGV;
+my ($dir, $log)=@ARGV;
 open(my $lfh, '>>', $log) or die("Can't open $log\n");
 ###############################
 # BEGIN
 my $rule=File::Find::Rule->file()->start($dir);
 while (defined(my $file=$rule->match)) {
-      my $orgi = $file;
-      my $sha = file_digest($file) or die "couldn't sha $file";
+      my $orgi=$file;
+      my $sha=file_digest($file) or die "couldn't sha $file";
       if ($sha eq $orgi) {
             next;
       }
@@ -23,8 +23,8 @@ while (defined(my $file=$rule->match)) {
       }
 }
 sub file_digest {
-    my ($filename) = @_;
-    my $digest = Digest::SHA->new(256);
+    my ($filename)=@_;
+    my $digest=Digest::SHA->new(256);
     $digest->addfile($filename, "b");
     return $digest->hexdigest();
 }
