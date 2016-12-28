4c74077fb0cd92e16137f1a13b82c0df2cc00548
Prince of Archives <rich.tijerina@gmail.com>
2016-12-27 19:59:34 -0700

    Update 2_Standard_Block.sh

--- 2_Standard_Block.sh
+++ 2_Standard_Block.sh
@@ -24,28 +24,27 @@ fi
 target=${1%/}
 dump=${2%/}
 
-[[ -d "$target" ]] || echo "INVALID SPLIT DIR"; exit 1; 
-[[ -d "$dump" ]] || echo "INVALID DUMP DIR"; exit 1; 
+[[ -d "$target" ]] || { echo "INVALID SPLIT DIR"; exit 1; }
+[[ -d "$dump" ]] || { echo "INVALID DUMP DIR"; exit 1; } 
 
 for line in $target/*
 do
-	wsha=$( sha256 "$target"/"$line" | awk '{ print $NF }' )
-	temp=""
-	split -a 6 -d -b 100000 "$target"/"$line" "$dump"/todo/;
-	touch "$dump"/key/$wsha;
+	printf "$line\n";
+	wsha=$( sha256 "$line" | awk '{ print $NF }' )
+	split -a 6 -d -b 100000 "$line" "$dump"/todo/;
+	touch "$dump"/key/"$wsha";
 	printf "" > "$dump"/temp;
 	list=$( ls "$dump"/todo/ )
 	for i in $list
 		do
-		psha=$( sha256 "$dump"/todo/$i | awk '{ print $NF }' )
-		printf "%s\n" "$psha" >> "$dump"/key/k$wsha;
+		psha=$( sha256 "$dump"/todo/"$i" | awk '{ print $NF }' )
+		printf "%s\n" "$psha" >> "$dump"/key/k"$wsha";
 		mv "$dump"/todo/"$i" "$dump"/processed/"$psha";
 		cat "$dump"/processed/"$psha" >> "$dump"/temp;
 		done
-	temp=$( sha256 "$dump"/temp | awk '{ print $NF }' )
-	if [ $wsha == $temp ]
-		then printf "SUCCESS $temp\n"
-		else printf "FAIL SHA RECHECK $tmp" >> "$dump"/fails
+	tmp=$( sha256 "$dump"/temp | awk '{ print $NF }' )
+	if [ "$wsha" == "$tmp" ]
+		then printf "SUCCESS $tmp\n";
+		else printf "FAIL SHA RECHECK $line" >> "$dump"/fails;
 	fi
 done
-
