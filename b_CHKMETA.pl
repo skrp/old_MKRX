ecf86e251e54d606aa1b366a8f788d6881341d17
Prince of Archives <rich.tijerina@gmail.com>
2016-12-30 23:55:48 -0700

    Update b_CHKMETA.pl

--- b_CHKMETA.pl
+++ b_CHKMETA.pl
@@ -1,17 +1,14 @@
 #!/usr/local/bin/perl
 # # METACHK - confirm uniq file has meta
 # by skrp of MKRX
-use strict;
-use warnings;
+use strict; use warnings;
 
 my ($fmaster, $mmaster) = @ARGV;
 open(my $ffp, '<', $fmaster) or die ("Couldn't open filemaster\n");
 open(my $mfp, '<', $mmaster) or die ("Couldn't open metamaster\n");
 
-my @file = readline $ffp;
-my @meta = readline $mfp;
-chomp @file;
-chomp @meta;
+my @file = readline $ffp; chomp @file;
+my @meta = readline $mfp; chomp @meta;
 
 my %mkrx = map{$_ => undef} @file;
 
@@ -21,14 +18,12 @@ foreach my $meta (@meta) {
 }
 # UNDEF SCAN
 while (my ($key, $value) = each %mkrx) {
-	if (not defined $mkrx{$key} ) { print "$key: NEEDS FILE\n"; }
+	if (not defined $mkrx{$key} )
+		{ print "$key: NEEDS FILE\n"; }
 }
 # COUNT
 print "SICC POOL COUNT:\n";
 my $size = keys %mkrx;
 print "$size\n";
-
-######################
-
 exit 0;
  
