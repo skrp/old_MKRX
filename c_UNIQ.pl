21a6091487ac788790085171fe7dc2cd63f4786d
Prince of Archives <rich.tijerina@gmail.com>
2016-12-30 23:56:25 -0700

    Update c_UNIQ.pl

--- c_UNIQ.pl
+++ c_UNIQ.pl
@@ -1,16 +1,14 @@
 #!/usr/local/bin/perl
 # UNIQ - cmp sha against uniq list
 # by skrp of MKRX
-use strict;
-use warnings;
+use strict; use warnings;
 my ($pmaster, $mmaster, $log) = @ARGV;
 open(my $pfp, '<', $pmaster) or die ("Couldn't open partialmaster\n");
 open(my $mfp, '<', $mmaster) or die ("Couldn't open master\n");
 open(my $lfp, '>>', $log) or die;
-my @pfile = readline $pfp;
-my @mfile = readline $mfp;
-chomp @pfile;
-chomp @mfile;
+my @pfile = readline $pfp; chomp @pfile;
+my @mfile = readline $mfp; chomp @mfile;
+
 my %mkrx = map{$_ => undef} @mfile;
 foreach my $p (@pfile) {
 	if (exists $mkrx{$p}) { next; }
