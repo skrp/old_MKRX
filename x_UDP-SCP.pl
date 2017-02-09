73fdd6a009f2ce62c8b643b7ba7ba56ca921de16
Prince of Archives <rich.tijerina@gmail.com>
2017-02-08 22:39:56 -0700

    Create x_UDP-SCP.pl

---
+++ x_UDP-SCP.pl
@@ -0,0 +1,9 @@
+#!/usr/local/bin/perl
+use strict; use warnings;
+use MKRX;
+use KRIP;
+######################
+# IPV6-UDP-SCP - mkrx proto
+0 -288 header
+288 - 400 system header first
+401 - payload
