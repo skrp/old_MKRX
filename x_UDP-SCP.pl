b5321997feb1ef2fa4738f5f0be33633d99572d7
Prince of Archives <rich.tijerina@gmail.com>
2017-02-08 22:40:49 -0700

    Delete x_UDP-SCP.pl

--- x_UDP-SCP.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/local/bin/perl
-use strict; use warnings;
-use MKRX;
-use KRIP;
-######################
-# IPV6-UDP-SCP - mkrx proto
-0 -288 header
-288 - 400 system header first
-401 - payload
