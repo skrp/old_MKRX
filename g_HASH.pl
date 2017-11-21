b73a7dd77018d3a16cbdc1cefec8870b58e7cb62
Prince of Archives <rich.tijerina@gmail.com>
2017-11-21 12:42:16 -0700

    Update g_HASH.pl

--- g_HASH.pl
+++ g_HASH.pl
@@ -1,24 +1,30 @@
 #!/usr/local/bin/perl
 use strict; use warnings;
 use File::Find::Rule;
-####################################
-# HASH - populate hash via metafiles
-####################### skrp of MKRX
-# SETUP ############################
-my ($target) = @ARGV;
-# test for last '/' of $targe
-my $rule = File::Find::Rule->file()->start($target);
-# comment out unused categories to reduce massive size in memory 
-my $nnfd = $target.'NAM';
-my $ppfd = $target.'PAT';
-my $zzfd = $target.'SIZ';
-my $eefd = $target.'ENC';
-open(my $nfd, '>>', $nnfd);
-open(my $pfd, '>>', $ppfd);
-open(my $zfd, '>>', $zzfd);
-open(my $efd, '>>', $eefd);
+######################################################
+# HASH - scrape metadata-files to metadata masterfiles
+########################################## skrp of MKRX
+# this process is io intensive
+# format of each metadata masterfile => "file-sha metadata-value\n"
+# EXAMPLE: "922440e6b538bc9e7dd72b58084d2712cd770f61f370261f5bf6528b5f8d3083 FreeBSD-10.3-RELEASE-amd64-memstick.img\n" >> NAM
 
-# POPULATE HASH METAFILES ###########
+# SETUP #############################################
+# ARG1 is the metadata directory
+my ($metadata_dir) = @ARGV;
+die "ARG1 metadata dir" if (!defined $metadata_dir);
+
+my $rule = File::Find::Rule->file()->start($metadata_dir);
+
+my $nmaster = 'NAM';
+open(my $nfd, '>>', $nmaster);
+my $pmaster = 'PAT';
+open(my $pfd, '>>', $pmaster);
+my $zmaster = 'SIZ';
+open(my $zfd, '>>', $zmaster);
+my $emaster = 'ENC';
+open(my $efd, '>>', $emaster);
+
+# POPULATE HASH METAFILES ##################
 while (defined(my $file = $rule->match)) {
 	open(my $fd, '<', $file); 
 	my @f = readline $fd;
