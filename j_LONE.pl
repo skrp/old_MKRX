8964d055ae44ceec192d6268d89ff04d9d153dbf
Prince of Archives <rich.tijerina@gmail.com>
2018-01-29 00:33:59 -0800

    Update and rename j_LONE to j_LONE.pl

--- j_LONE.pl
+++ j_LONE.pl
@@ -1,6 +1,9 @@
 use strict; use warnings;
 use File::Find::Rule;
 
+#########################################
+# LONE - discover unduplicated files
+
 my ($host_path) = @ARGV;
 
 while (defined File::Find::Rule->file()->in($host_path))
