615182b89565b9182fc536eb4f6dcbbd8110a216
Prince of Archives <rich.tijerina@gmail.com>
2018-01-29 00:38:35 -0800

    Update j_LONE.pl

--- j_LONE.pl
+++ j_LONE.pl
@@ -8,3 +8,20 @@ my ($host_path) = @ARGV;
 
 while (defined File::Find::Rule->file()->in($host_path))
 {
+  open(my $ifh, '<', $_);
+  my @i = readline $ifh; 
+  close $ifh; chomp @i;
+  for (@i)
+  { 
+# CLONE COUNT ###########################
+    my $c = $LIST{$_};
+    $c++;
+    $LIST{$_} = $c;
+  }
+}
+# STDOUT ################################
+for (keys %LIST)
+{
+  my $c = $LIST{$_};
+  print "$_\n" if ($i == 1);
+}
