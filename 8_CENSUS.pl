f3da39d22547910f1d30f8b1e7de79157ba7030b
Prince of Archives <rich.tijerina@gmail.com>
2018-01-29 00:27:23 -0800

    Update 8_CENSUS.pl

--- 8_CENSUS.pl
+++ 8_CENSUS.pl
@@ -2,9 +2,27 @@
 use strict; use warnings;
 use File::Find::Rule;
 
+#####################################################
+# CENSUS - locations of files
+
+# $sha $host $host $host ...
+
 my ($host_path) = @ARGV;
 die "ARG1 host_path" if (!defined $host_path);
 
 my %LIST;
 
-while (defined File::Find::Rule->file()->ine($host_path);
+while (defined File::Find::Rule->file()->ine($host_path)
+{
+  my $host = $_;
+  $host =~ s?.*\/??;
+  $host =~ s?_.*??;
+  
+  open(my $ifh, '<', $_);
+  my @i = readline $ifh;
+  close $ifh; chomp @i;
+  
+  $LIST{$_} .= " $host" for (@i);
+} 
+
+print "$_ $LIST{$_}\n" for (keys %LIST);
