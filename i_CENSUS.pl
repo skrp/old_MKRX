0d13010d1c0e89e6449b3f3c8da3272c6667c67e
Prince of Archives <rich.tijerina@gmail.com>
2018-07-08 11:16:55 -0600

    Update i_CENSUS.pl

--- i_CENSUS.pl
+++ i_CENSUS.pl
@@ -2,35 +2,26 @@
 use strict; use warnings;
 #####################################################
 # CENSUS - locations of files
-
-# List all mkrx storage nodes
-# ex: bkup11_LIST
-
-# Each node with a newline delimited file list
-# FORMAT:
-#        $sha $host $host $host ...
-
-my ($host_path) = @ARGV;
-die "ARG1 host_path" if (!defined $host_path);
-
+#####################################################
 my %LIST;
-my @list = glob("$host_path/*_LIST");
-
-shift @list; shift @list; # rm . ..
-
+my @list;
+#####################################################
+@list = glob("/net/*_LIST");
+shift @list; shift @list;
+#####################################################
 for (@list)
 {
-  my $host = $_;
-  $host =~ s?.*\/??;
-  $host =~ s?_.*??;
-  
-  open(my $ifh, '<', $_);
+  my $ifile = $_;
+  open(my $ifh, '<', $ifile);
   my @i = readline $ifh;
   close $ifh; chomp @i;
-  
-  my $date = shift @i;
-  
-  $LIST{$_} .= " $host_$date" for (@i);
-} 
-
+#####################################################
+  for (@i)
+  {
+    my $iline = $_;
+    my @line = split(" ", 2, $iline);
+    $LIST{$line[0]} .= " $line[1]";
+  }
+}
+#####################################################
 print "$_$LIST{$_}\n" for (keys %LIST);
