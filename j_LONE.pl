f155fc7cb58ec58364cd4d7ad8fe9bc8f6e336b7
Prince of Archives <rich.tijerina@gmail.com>
2018-07-08 11:29:13 -0600

    Update j_LONE.pl

--- j_LONE.pl
+++ j_LONE.pl
@@ -1,19 +1,19 @@
 use strict; use warnings;
 ##############################################
 # LONE - discover unduplicated files
+##############################################
 my @lines;
 my $hn = system("hostname");
-
+##############################################
 open(my $cfh, '<', "/net/$hn");
 @lines = readline $cfh;
 close $cfh; chomp @lines;
-
+##############################################
 for (@lines)
 {
   my @array = split(" ", $_};
   my $cnt =  0;
-  
+##############################################
   $cnt++ for (@array);
-#  print "$array[0]\n" if (($cnt == 2) && ($array[1] eq $pool));
   print "$array[0] $array[1]\n" if ($cnt == 2);
 }
