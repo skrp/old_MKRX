02727f3b159001bb278361d7d7ddad7c2d609f80
Prince of Archives <rich.tijerina@gmail.com>
2018-07-08 11:09:40 -0600

    Update a_SCRUB.pl

--- a_SCRUB.pl
+++ a_SCRUB.pl
@@ -1,19 +1,33 @@
 #!/usr/local/bin/perl
 use strict; use warnings;
-use Digest::SHA ();
+use Digest::SHA;
 #######################################
 # SCRUB - output sha-stamped-lists
 my ($pool)=@ARGV;
-open(my $lfh, '>>', $log) or die("Can't open $log\n");
-###############################
-# BEGIN
-my $rule=File::Find::Rule->file()->start($dir);
-while (defined(my $file=$rule->match)) {
-      my $sha=file_digest($file) or die "couldn't sha $file";
+die "ARG1 pool-name" if (!defined $pool);
+my $date = time;
+my $stamp = $pool.'_'.$date;
+my $new = '/net/'.$pool.'_LIST';
+#######################################
+open(my $nfh, '>', $new);
+#######################################
+opendir(my $dh, $dir) or die "FAIL opendir $dir";
+my @list = readdir $dh;
+closedir $dh; chomp @list;
+#######################################
+for (@list)
+{
+	my $file = $_;
+	my $sha=file_digest($file) or die "couldn't sha $file";
+#######################################
 	$file =~ s/.*\///;
-      if ($sha eq $file) { next; }
-      else {  print {$lfh} "$file: ALERT FKN ENTROPY!\n"; }
+#######################################
+      	if ($sha ne $file) 
+		{ print "FAIL $file\n"; next; }
+#######################################	
+	print $nfh "$file $stamp\n";
 }
+#######################################
 sub file_digest {
     my ($filename)=@_;
     my $digest=Digest::SHA->new(256);
