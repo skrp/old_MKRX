c787dccfc94b025e859724b72f517ce2e6f46b7e
Prince of Archives <rich.tijerina@gmail.com>
2018-11-09 15:28:32 -0800

    Create q_initNEST.pl

---
+++ q_initNEST.pl
@@ -0,0 +1,6 @@
+#!/usr/local/bin/perl
+use strict; use warnings;
+#################################
+# initNEST - create the nested location
+#################################
+my ($target) = @ARGV;
