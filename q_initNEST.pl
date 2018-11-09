599113463246fad39bdf737be105e628286fcd4a
Prince of Archives <rich.tijerina@gmail.com>
2018-11-09 15:30:41 -0800

    Update q_initNEST.pl

--- q_initNEST.pl
+++ q_initNEST.pl
@@ -4,3 +4,14 @@ use strict; use warnings;
 # initNEST - create the nested location
 #################################
 my ($target) = @ARGV;
+die "ARG1 target\n" unless (defined $target);
+$target =~ s%/\z%%;
+#################################
+my @one = qw(a b c d e f 1 2 3 4 5 6 7 8 9 0);
+my @two = qw(a b c d e f 1 2 3 4 5 6 7 8 9 0);
+#################################
+for (one)
+{
+  my $i = $target.'/'.$_;
+  system("mkdir $i$_") for (@two);
+}
