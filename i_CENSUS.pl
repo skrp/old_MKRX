60c975442b9f9362d09d65d0a4dbebd60a22a4fb
Prince of Archives <rich.tijerina@gmail.com>
2018-11-09 17:50:32 -0800

    Update i_CENSUS.pl

--- i_CENSUS.pl
+++ i_CENSUS.pl
@@ -2,11 +2,17 @@
 use strict; use warnings;
 #####################################################
 # CENSUS - locations of files
+# each disk node lists one file per line
+#    example: vu2_LIST -> aa12...\naa2b..
+#####################################################
+my ($dir) = @ARGV;
+die "ARG1 dir\n" unless (defined $dir);
+$dir =~%/\z%%;
 #####################################################
 my %LIST;
 my @list;
 #####################################################
-@list = glob("/net/*_LIST");
+@list = glob("$dir/*_LIST");
 shift @list; shift @list;
 #####################################################
 for (@list)
@@ -16,11 +22,14 @@ for (@list)
   my @i = readline $ifh;
   close $ifh; chomp @i;
 #####################################################
+  my $node = $ifile;
+  $node =~ s/_LIST//;
+  $node =~ s%.*/%%;
+#####################################################
   for (@i)
   {
     my $iline = $_;
-    my @line = split(" ", 2, $iline);
-    $LIST{$line[0]} .= " $line[1]";
+    $LIST{$iline} .= " $node";
   }
 }
 #####################################################
